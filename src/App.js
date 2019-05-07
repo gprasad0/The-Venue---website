a718ecfd80a50427d2175a97f28932d5396f0170
guru <guru@foodmonk.com>
2019-05-07 23:50:15 +0530

    changes added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./resources/styles.css";
-import Header from './components/header_footer/Header';
-import Featured from './components/featured/index';
+import Header from "./components/header_footer/Header";
+import Featured from "./components/featured/index";
+import VenueInfo from "./components/venueInfo/VenueInfo";
 function App() {
   return (
-    <div className="App" style={{height:"1000px",background:"red"}}>
+    <div className="App" style={{ height: "1000px" }}>
       <Header />
       <Featured />
+      <VenueInfo />
     </div>
   );
 }
